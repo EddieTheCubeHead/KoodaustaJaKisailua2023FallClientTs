6059f37658150acec050a6bfa166ebc2e1c46ba3
Eetu Asikainen <eetu.asikainen@visma.com>
2023-11-30 12:49:40 +0200

    Document helpers, add function to get own ship id

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,14 @@
 import {CellType, CompassDirection, GameMap, GridPosition} from "./wrapper/models";
+import {config} from "./config_provider";
 
+/**
+ * Get the difference between two coordinates
+ *
+ * @param origin the origin (source) coordinates for the calculation
+ * @param target the target coordinates for the calculation
+ *
+ * @returns a vector representing the difference from origin to target
+ */
 export const getCoordinateDifference = (origin: GridPosition, target: GridPosition): GridPosition => {
     return {x: target.x - origin.x, y: target.y - origin.y}
 }
@@ -9,6 +18,13 @@ const getVectorAngleDegrees = (vector: GridPosition) => {
     return angleDegrees < 0 ? angleDegrees + 360 : angleDegrees
 }
 
+/**
+ * Get a compass direction most closely representing the given vector
+ *
+ * @param vector the vector which should be converted to approximate compass direction
+ *
+ * @returns the compass direction closest to the vector
+ */
 export const getApproximateDirection = (vector: GridPosition): CompassDirection => {
     const angle = getVectorAngleDegrees(vector)
     const cutoff = 360 / 16
@@ -30,6 +46,14 @@ export const getApproximateDirection = (vector: GridPosition): CompassDirection 
     return CompassDirection.northWest
 }
 
+/**
+ * Get coordinates for a given entity from the given game map
+ *
+ * @param entityId the id of the entity to search for in the map
+ * @param gameMap the game map to search for the entity in
+ *
+ * @returns the entity coordinates if the entity exists, otherwise null
+ */
 export const getEntityCoordinates = (entityId: string, gameMap: GameMap): GridPosition | null => {
     for (const [y, row] of gameMap.entries()) {
         for (const [x, cell] of row.entries()) {
@@ -65,6 +89,18 @@ const directionByIndex = [
     CompassDirection.southWest
 ]
 
+/**
+ * Get the compass direction that is the furthest one you are allowed to turn towards from the given starting
+ * direction, given the turn rate.
+ *
+ * @param startingDirection the starting direction for the turn
+ * @param targetDirection the target direction for the turn
+ * @param turnRate the turn rate for the game, see models.ClientContext.turnRate
+ *
+ * @returns The furthest direction between starting and target directions allowed by the turn rate
+ *
+ * @remarks If performing a 180-degree turn, the function will always perform the partial turn clockwise
+ */
 export const getPartialTurn = (startingDirection: CompassDirection, targetDirection: CompassDirection, turnRate: number) => {
     let initialTurn = directionMappings[targetDirection] - directionMappings[startingDirection]
     let afterTurnDirection: number
@@ -75,4 +111,13 @@ export const getPartialTurn = (startingDirection: CompassDirection, targetDirect
         afterTurnDirection = directionMappings[startingDirection] + Math.min(initialTurn, turnRate)
     }
     return afterTurnDirection < 0 ? directionByIndex[afterTurnDirection + 8] : directionByIndex[afterTurnDirection % 8]
+}
+
+/**
+ * Get the id of your ship
+ *
+ * @returns The id of your ship as str
+ */
+export const getOwnShipId = () => {
+    return `ship:${config.token}:${config.botName}`
 }
